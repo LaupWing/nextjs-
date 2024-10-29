263b50a0ac5c26ebdaa070d3343877c08aca1162
laupwing <laupwing@gmail.com>
2024-10-29 19:41:47 +0100

    Refactor code to remove unused imports and console logs

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,16 +3,6 @@ import Stripe from "stripe"
 // import { PrismaClient } from "@prisma/client"
 import nodemailer from "nodemailer"
 
-// const prisma = new PrismaClient()
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2024-09-30.acacia",
-})
-
-console.log({
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-})
-
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT!),
