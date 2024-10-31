92609b942ea8df3094f0cbd7ef4c840b5b50b9e6
laupwing <laupwing@gmail.com>
2024-10-31 15:07:07 +0100

    Refactor email handling to use SendGrid and update attachment processing for workout plan ebook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,21 +1,14 @@
 import { NextResponse } from "next/server"
 import Stripe from "stripe"
-// import { PrismaClient } from "@prisma/client"
-import nodemailer from "nodemailer"
+import sgMail from "@sendgrid/mail"
 import path from "path"
+import fs from "fs/promises"
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
 
 // const prisma = new PrismaClient()
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: "2024-09-30.acacia",
-})
-
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: parseInt(process.env.EMAIL_PORT!),
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
 })
 
 export async function POST(req: Request) {
@@ -23,7 +16,6 @@ export async function POST(req: Request) {
     const sig = req.headers.get("stripe-signature")!
 
     let event: Stripe.Event
-    console.log(process.env.STRIPE_WEBHOOK_SECRET!)
     try {
         event = stripe.webhooks.constructEvent(
             buf,
@@ -40,44 +32,60 @@ export async function POST(req: Request) {
 
     console.log("Received event", event.type)
     if (event.type === "checkout.session.completed") {
-        // console.log("Received event", event)
         const session = event.data.object as Stripe.Checkout.Session
         const customerEmail = session.customer_details?.email
 
         if (customerEmail) {
-            // Create database record
-            console.log("Creating user record for", customerEmail)
-            // await prisma.user.create({
-            //     data: {
-            //         email: customerEmail,
-            //     },
-            // })
+            const attachmentPath = path.join(
+                process.cwd(),
+                "assets",
+                "workout-plan-ebook.pdf"
+            )
+            const fileContent = await fs.readFile(attachmentPath)
+            const base64File = fileContent.toString("base64")
             console.log({
                 from: process.env.EMAIL_FROM,
-                to: customerEmail,
-                subject: "Body Craft System Ebook",
-                text: "Thank you for your purchase! Here is your ebook. Reply to this email to book a call with me!.",
+                to: "laupwing@gmail.com",
+                subject: "Your Workout Plan Ebook",
+                text: "Thank you for your purchase! Here is your ebook.",
+                attachments: [
+                    {
+                        filename: "workout-plan-ebook.pdf",
+                        path: attachmentPath,
+                    },
+                ],
             })
+            const msg = {
+                to: "laupwing@gmail.com",
+                from: process.env.EMAIL_FROM!,
+                subject: "Your Workout Plan Ebook",
+                text: "Thank you for your purchase! Here is your ebook.",
+                attachments: [
+                    {
+                        content: base64File,
+                        filename: "workout-plan-ebook.pdf",
+                        type: "application/pdf",
+                        disposition: "attachment",
+                    },
+                ],
+            }
             try {
-                // Send email
-                await transporter.sendMail({
-                    from: process.env.EMAIL_FROM,
+                await sgMail.send({
+                    from: process.env.EMAIL_FROM!,
                     to: customerEmail,
                     subject: "Body Craft System Ebook",
                     text: "Thank you for your purchase! Here is your ebook. Reply to this email to book a call with me!.",
                     attachments: [
                         {
+                            content: base64File,
                             filename: "workout-plan-ebook.pdf",
-                            path: path.join(
-                                process.cwd(),
-                                "assets",
-                                "workout-plan-ebook.pdf"
-                            ),
+                            type: "application/pdf",
+                            disposition: "attachment",
                         },
                     ],
                 })
-                await transporter.sendMail({
-                    from: process.env.EMAIL_FROM,
+                await sgMail.send({
+                    from: process.env.EMAIL_FROM!,
                     to: "laupwing@gmail.com",
                     subject: "New Order",
                     text: `New order from ${customerEmail}`,
